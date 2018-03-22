720efcd4cf0e0504df5838d382cc4b9990f6d015
sea2 <497661292@qq.com>
2018-03-22 17:58:31 +0800

    粘性标题

---
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/recyclerview/RecyclerViewHomeActivity.java
@@ -0,0 +1,122 @@
+package com.chad.baserecyclerviewadapterhelper.recyclerview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.chad.baserecyclerviewadapterhelper.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 系统提供的三种LayoutManager可以无缝衔接ListView和GridView，瀑布流的实现也变得无分简单。滑动删除和长按交换只需要添加几个类就可以实现。
+ * <p>
+ * 除此之外，RecyclerView的动画配以局部刷新也是它比较出色的地方
+ * <p>
+ * 回收 ViewHolder  复用convertview
+ * <p>
+ * CardView  http://www.jianshu.com/p/33b1d21d6ba6
+ * 其次介绍一下它的基本属性：
+ * app:cardBackgroundColor这是设置背景颜色
+ * app:cardCornerRadius这是设置圆角大小
+ * app:cardElevation这是设置z轴的阴影
+ * app:cardMaxElevation这是设置z轴的最大高度值
+ * app:cardUseCompatPadding是否使用CompatPadding
+ * app:cardPreventCornerOverlap是否使用PreventCornerOverlap
+ * app:contentPadding 设置内容的padding
+ * app:contentPaddingLeft 设置内容的左padding
+ * app:contentPaddingTop 设置内容的上padding
+ * app:contentPaddingRight 设置内容的右padding
+ * app:contentPaddingBottom 设置内容的底padding
+ */
+public class RecyclerViewHomeActivity extends ActionBarActivity {
+
+    private RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private RecyclerViewHomeAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_recyclerview);
+
+        initData();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.id_recyclerview);
+        mAdapter = new RecyclerViewHomeAdapter(this, mDatas);
+
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
+        // 设置item动画
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        initEvent();
+
+    }
+
+    private void initEvent() {
+        mAdapter.setOnItemClickLitener(new RecyclerViewHomeAdapter.OnItemClickLitener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(RecyclerViewHomeActivity.this, position + " click", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(RecyclerViewHomeActivity.this, position + " long click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    protected void initData() {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_action_add:
+                mAdapter.addData(1);
+                break;
+            case R.id.id_action_delete:
+                mAdapter.removeData(1);
+                break;
+            case R.id.id_action_gridview:
+                mRecyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+                break;
+            case R.id.id_action_listview:
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+                break;
+            case R.id.id_action_horizontalGridView:
+                mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.HORIZONTAL));
+                break;
+
+            case R.id.id_action_staggeredgridview:
+                Intent intent = new Intent(this, StaggeredGridLayoutActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+}
